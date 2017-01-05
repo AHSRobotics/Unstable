d646d6489bff63c5ea444efc9c9e043320ef6927
BenTibnam <BenTibnam@users.noreply.github.com>
2017-01-05 14:13:48 -0500

    No Killswitch

--- TeamOneMain.java
+++ TeamOneMain.java
@@ -6,18 +6,17 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import java.util.InputMismatchException;
 
 
-@TeleOp(name = "Team One 0.4.1", group = "Unstable Test")
+@TeleOp(name = "Team One 0.4.3", group = "Unstable Test")
 
 public class TeamOneMain extends LinearOpMode{
 
     DcMotor motorLeft;
     DcMotor motorRight;
     DcMotor motorMiddle;
-    DcMotor motorPoker;
+    DcMotor motorLauncher;
 
     final double INCREMENTER = 0.01; // Used to control the strafing wheel
-    private boolean killSwitch = false; // Will determine if the motors will run
-    private DcMotor[] allMotors = {motorLeft, motorRight, motorMiddle, motorPoker}; // Used to shut down all of the motors
+
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -26,7 +25,7 @@ public class TeamOneMain extends LinearOpMode{
         motorLeft = hardwareMap.dcMotor.get("motorLeft");
         motorRight = hardwareMap.dcMotor.get("motorRight");
         motorMiddle = hardwareMap.dcMotor.get("motorMiddle");
-        motorPoker = hardwareMap.dcMotor.get("motorPoker");
+        motorLauncher = hardwareMap.dcMotor.get("motorSweep");
 
         motorRight.setDirection(DcMotor.Direction.REVERSE);
 
@@ -36,79 +35,45 @@ public class TeamOneMain extends LinearOpMode{
         double middleSpeed = 0.0;
 
         while (opModeIsActive()) {
-            if(!killSwitch) {
 
-                motorRight.setPower(-gamepad1.right_stick_y);
-                motorLeft.setPower(-gamepad1.left_stick_y);
 
-                /* Middle Strafing Controls */
-                if (gamepad2.dpad_left) {
-                    middleSpeed -= INCREMENTER;
+            motorRight.setPower(-gamepad1.right_stick_y);
+            motorLeft.setPower(-gamepad1.left_stick_y);
 
-                    if (middleSpeed >= -1.0) {
-                        motorMiddle.setPower(middleSpeed);
-                    } else {
-                        middleSpeed = -1.0;
-                    }
+            /* Middle Strafing Controls */
+            if (gamepad2.dpad_left) {
+                middleSpeed -= INCREMENTER;
 
-                }
-
-                if (gamepad2.dpad_right) {
-                    middleSpeed += INCREMENTER;
-
-                    if (middleSpeed <= 1.0) {
-                        motorMiddle.setPower(middleSpeed);
-                    } else {
-                        middleSpeed = 1.0;
-                    }
-                }
-
-                // Turns off the motor
-                if (!gamepad2.dpad_left && !gamepad2.dpad_right) {
-                    middleSpeed = 0; // Has to be to zero to create the acceleration effect
+                if (middleSpeed >= -1.0) {
                     motorMiddle.setPower(middleSpeed);
+                } else {
+                    middleSpeed = -1.0;
                 }
 
-
-               /* The Poker */
-
-                // Turning off the controller if nothing is being pressed
-                if (!gamepad2.a && !gamepad2.b) {
-                    motorPoker.setPower(0);
-
-                    // Some safety checking
-                    if (motorPoker.getPower() != 0.0) {
-                        telemetry.addData("Condition: ", "UNSAFE POWER: " + Double.toString(motorPoker.getPower()));
-                    } else {
-                        telemetry.addData("Condition: ", "SAFE POWER: " + Double.toString(motorPoker.getPower()));
-                    }
-
-                    telemetry.update();
-                }
-
-                if (gamepad2.a) {
-                    motorPoker.setPower(1.0);
-                }
-
-                // Putting the poker back in
-
-                if (gamepad2.b) {
-                    motorPoker.setPower(-1.0);
-                }
-
-
-            }else{
-                printMessage("WARNING: ", "THE KILLSWITCH HAS BEEN ACTIVATE, ALL MOTORS STOP. DEACTIVATE KILLSWITCH TO CONTINUE DRIVING");
             }
 
-            /* Killswitch */
-            if(gamepad1.back && gamepad1.start && gamepad1.a || gamepad2.back && gamepad2.start && gamepad2.a){
-                flipKillSwitch();
+            if (gamepad2.dpad_right) {
+                middleSpeed += INCREMENTER;
 
-                for(DcMotor motor : this.allMotors){
-                    motor.setPower(0.0);
+                if (middleSpeed <= 1.0) {
+                    motorMiddle.setPower(middleSpeed);
+                } else {
+                    middleSpeed = 1.0;
                 }
             }
+
+            // Turns off the motor
+            if (!gamepad2.dpad_left && !gamepad2.dpad_right) {
+                middleSpeed = 0; // Has to be to zero to create the acceleration effect
+                motorMiddle.setPower(middleSpeed);
+            }
+
+
+            /* The Sweeper */
+            if(gamepad2.right_trigger > 0)
+                motorLauncher.setPower(1.0);
+            else if(gamepad2.right_trigger <= 0)
+                motorLauncher.setPower(0);
 
             idle();
         }
@@ -125,11 +90,6 @@ public class TeamOneMain extends LinearOpMode{
             telemetry.update();
         }
     }
-
-    public void flipKillSwitch(){
-        this.killSwitch = (this.killSwitch) ? false : true;
-    }
-
 }
 
 /*
--- TeamTwoMain.java
+++ TeamTwoMain.java
@@ -5,16 +5,13 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import java.util.InputMismatchException;
 
-@TeleOp(name = "Team Two 0.2.0", group = "Unstable Test")
+@TeleOp(name = "Team Two 0.2.2", group = "Unstable Test")
 
 public class TeamTwoMain extends LinearOpMode{
 
     DcMotor motorLeft;
     DcMotor motorRight;
     DcMotor sweeper;
-
-    private boolean killSwitch = false;
-    private DcMotor[] allMotors = {motorLeft, motorRight, sweeper};
 
     @Override
     public void runOpMode() throws InterruptedException{
@@ -30,35 +27,21 @@ public class TeamTwoMain extends LinearOpMode{
         waitForStart();
 
         while(opModeIsActive()){
-            if(!killSwitch) {
-                motorLeft.setPower(-gamepad1.left_stick_y);
-                motorRight.setPower(-gamepad1.right_stick_y);
+            motorLeft.setPower(-gamepad1.left_stick_y);
+            motorRight.setPower(-gamepad1.right_stick_y);
 
-                /* Controlling sweeper motor */
-                if (gamepad1.left_trigger > 0.0) {
-                    sweeper.setDirection(DcMotor.Direction.FORWARD); // Need to change direction based on button pressed
-                    sweeper.setPower(-gamepad1.left_trigger);
-                }
-
-                if (gamepad1.right_trigger > 0.0) {
-                    sweeper.setDirection(DcMotor.Direction.REVERSE);
-                    sweeper.setPower(-gamepad1.right_trigger);
-                }
-            }else{
-                printMessage("WARNING: ", "THE KILLSWITCH HAS BEEN ACTIVATE, ALL MOTORS STOP. DEACTIVATE KILLSWITCH TO CONTINUE DRIVING");
+            /* Controlling sweeper motor */
+            if (gamepad1.left_trigger > 0.0) {
+                sweeper.setDirection(DcMotor.Direction.FORWARD); // Need to change direction based on button pressed
+                sweeper.setPower(-gamepad1.left_trigger);
             }
 
-            /* Killswitch */
-            if(gamepad1.back && gamepad1.start && gamepad1.a || gamepad2.back && gamepad2.start && gamepad2.a){
-                flipKillSwitch();
-
-                for(DcMotor motor : this.allMotors){
-                    motor.setPower(0.0);
-                }
+            if (gamepad1.right_trigger > 0.0) {
+                sweeper.setDirection(DcMotor.Direction.REVERSE);
+                sweeper.setPower(-gamepad1.right_trigger);
             }
 
             idle();
-
         }
 
     }
@@ -72,10 +55,6 @@ public class TeamTwoMain extends LinearOpMode{
         catch(InputMismatchException e){
             telemetry.addData("WARNING: ", "A call to \"printMessage\" has an incorrect data type entered as one of the arguments. Talk to one of the programmers to fix the problem.");
         }
-    }
-
-    public void flipKillSwitch(){
-        this.killSwitch = (this.killSwitch) ? false : true;
     }
 }
 
